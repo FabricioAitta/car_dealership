44bc8ad7a5f999f48aa6d20122a39b59a5de8b5b
FabricioAitta <alexanderaitta@gmail.com>
2021-09-28 14:19:07 -0300

    sidenav admin sin terminar

---
+++ pages/admin.js
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+import React, { useEffect, useState } from 'react'
+import Loader from '../components/utils/loader';
+import { ENDPOINT } from '../utils';
+import Admin from '../components/admin';
+
+export default function Home({cars, brand}) {
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    },1500)
+  },[])
+
+  return (
+    <div className="container max-w-full">
+      <Head>
+        <title>Nort Motors Automoviles</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      {loading
+      ?
+      <Loader />
+      :
+      <>
+      <Admin />
+      </>
+      }
+    </div>
+  )
+}
+
+// export async function getServerSideProps() {
+//   const res = await fetch(`${ENDPOINT}/cars`)
+//   const cars = await res.json()
+
+//   const brand_res = await fetch(`${ENDPOINT}/brand`)
+//   const brand = await brand_res.json()
+
+//   return {
+//     props: {
+//       cars,
+//       brand,
+//     },
+//   }
+// }
